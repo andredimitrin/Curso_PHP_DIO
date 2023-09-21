604262072ca04c035988c6ecf7436b9735bc9bd1
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-09-21 20:10:07 -0300

    atualização1

---
+++ tipos_dados/array.php
@@ -0,0 +1,9 @@
+<?php
+
+$cursos = $arrayName = array( 'PHP' => 'Programação em PHP', 'HTML' => 'Linguagem de Marcação', 'CSS' => 'Linguagem de Marcação', 'JAVASCRIPT' => 'Linguagem de Marcação');
+
+print_r($cursos['PHP']);
+
+
+
+?>
---
+++ tipos_dados/srings.php
@@ -0,0 +1,26 @@
+<?php
+
+/*$s = "Curso de PHP";
+
+$valorDia = "80";
+$gorjetas = "15";
+
+$soma = $valorDia + $gorjetas;
+
+echo $s;
+echo $soma; */
+
+
+$texto = "<h1>Curso de PHP</h1>";
+$h4 = "<h4>Curso de PHP</h4>";
+$h5 = "<h5>Curso de PHP</h5>";
+
+echo $texto;
+echo $h4;
+echo $h5;
+
+
+
+
+
+?>
