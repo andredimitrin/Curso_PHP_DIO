107e3adf63bd4bcef865cc0f2071e59beea0d597
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-12-07 23:27:46 -0300

    atualizar

---
+++ poo/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9de65bc906fe134ad01a18dd05cc64e1d4d756c1',
+        'name' => 'andre/poo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'andre/poo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9de65bc906fe134ad01a18dd05cc64e1d4d756c1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
